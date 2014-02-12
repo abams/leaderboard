5f69eea5e3bf04795b05015eca5046efa8dc3ea2
Adam Ryan <adam.g.ryan@gmail.com>
2014-02-12 11:49:03 -0800

    turn off csrf protection for api

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::BaseController < ApplicationController
   include Api::V1::UsersHelper
   include Api::V1::RackHelper
 
+  protect_from_forgery with: :null_session
   before_filter :require_user
 end
