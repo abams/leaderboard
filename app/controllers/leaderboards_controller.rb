bbb87ce534aa321e21eac25c20d2a94f57301f09
Adam Ryan <adam.g.ryan@gmail.com>
2014-02-28 16:28:42 -0800

    temporary ranking hack

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -2,5 +2,9 @@ class LeaderboardsController < ApplicationController
 
 	def index
 		@rankings = Ranking.current_month
+
+    if @rankings.empty?
+      @rankings = Ranking.where(month: 1.month.ago.strftime('%Y%m')).order(score: :desc)
+    end
 	end
 end
